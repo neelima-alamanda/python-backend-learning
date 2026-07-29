a66084e1098ebaff38f290f504c228bf3600d376
neelima-alamanda <neelimaalamanda2005@gmail.com>
2026-07-29 10:54:15 +0530

    Add Day 07 Python File Handling notebook

---
+++ Day07_Python_File_Handling/calculator.py
@@ -0,0 +1,15 @@
+# Add two numbers
+def add(a, b):
+    return a + b
+
+# Subtract two numbers
+def subtract(a, b):
+    return a - b
+
+# Multiply two numbers
+def multiply(a, b):
+    return a * b
+
+# Divide two numbers
+def divide(a, b):
+    return a / b
